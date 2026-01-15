f10846d2f53ca6c104bf8a869cd00d83a8963a7a
Myth <mcs252102@iitd.ac.in>
2026-01-15 20:46:11 +0530

    Iterative allocation and simple metric added

--- src/vm.h
+++ src/vm.h
@@ -4,12 +4,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 1024
 #define MEM_SIZE 256
 #define CODE_SIZE 4096
 #define CALLSTACK_SIZE 256
-
 
 typedef enum {
     VAL_INT,
@@ -18,7 +18,8 @@ typedef enum {
 
 typedef enum {
     OBJ_PAIR,
-    
+    OBJ_FUNCTION, 
+    OBJ_CLOSURE   
 } ObjType;
 
 struct Obj; 
@@ -31,19 +32,30 @@ typedef struct {
     } as;
 } Value;
 
+
 typedef struct Obj {
     ObjType type;
-    bool marked;      // For GC Mark phase
-    struct Obj* next; // Linked list of all allocated objects
+    bool marked;      
+    struct Obj* next; 
 } Obj;
 
 typedef struct {
-    Obj obj; // Header
+    Obj obj;
     Value head;
     Value tail;
 } ObjPair;
 
-// Macros for ease of use
+typedef struct {
+    Obj obj;
+    int arity;
+} ObjFunction;
+
+typedef struct {
+    Obj obj;
+    ObjFunction* function;
+    struct Obj* upvalues; 
+} ObjClosure;
+
 #define IS_OBJ(v) ((v).type == VAL_OBJ)
 #define IS_INT(v) ((v).type == VAL_INT)
 #define AS_OBJ(v) ((v).as.obj)
@@ -52,12 +64,9 @@ typedef struct {
 #define VAL_OBJ(o) ((Value){VAL_OBJ, {.obj = (Obj*)o}})
 #define VAL_INT(i) ((Value){VAL_INT, {.integer = i}})
 
-
 typedef struct {
-    // Stack and Memory now hold Values
     Value stack[STACK_SIZE]; 
     Value memory[MEM_SIZE];
-    
     int32_t callstack[CALLSTACK_SIZE];
     uint8_t code[CODE_SIZE];
 
@@ -66,22 +75,34 @@ typedef struct {
     int csp;
     int running;
     int error;
+    bool debug_gc;             
+    unsigned long total_freed; 
+    int gc_run_count;
 
     // GC State
-    Obj* objects;   // Head of the linked list of all heap objects
-    int numObjects; // Current count of objects
+    Obj* objects;   
+    int numObjects; 
     int maxObjects; 
 
-   
+    Obj** grayStack;
+    int grayCount;
+    int grayCapacity;
+
     unsigned long instr_count;
     unsigned long byte_count;
-
 } VM;
 
 void vm_init(VM *vm);
+void vm_free(VM *vm); 
 void vm_load(VM *vm, const char *filename);
 void vm_run(VM *vm);
 
+void gc(VM* vm);
+void push(VM *vm, Value v); 
+Value pop(VM *vm);          
+
 Obj* new_pair(VM* vm, Value head, Value tail);
+ObjFunction* new_function(VM* vm);
+ObjClosure* new_closure(VM* vm, ObjFunction* func, Obj* upvalues);
 
 #endif
